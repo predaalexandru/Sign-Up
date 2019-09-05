8455a2eda13e4ae5327b14b4dbcfba265e0635b4
predaalexandru <alexpreda95@yahoo.com>
2019-09-05 17:30:29 +0300

    Making style changes to the Login page

--- sign-up/src/Login.js
+++ sign-up/src/Login.js
@@ -19,6 +19,7 @@ class Login extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  // call function signInWIthEmailAndPassword() to pass in the email and password
   login(e) {
     e.preventDefault();
     fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
@@ -27,6 +28,7 @@ class Login extends Component {
       });
   }
 
+  //create the user with the specified email and password
   signup(e){
     e.preventDefault();
     fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
@@ -37,21 +39,28 @@ class Login extends Component {
   }
   render() {
     return (
-      <div className="col-md-6">
-        <form>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input  value={this.state.email} onChange={this.handleChange} type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+      <div className="panel">
+         <form className="form-horizontal">
+          <div className="control-group">
+            <label className="control-label" forhtml="inputEmail">Email address</label>
+            <div className="controls">
+            <input  value={this.state.email} onChange={this.handleChange} type="text" id="inputEmail" placeholder="Enter email" />
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input  value={this.state.password} onChange={this.handleChange} type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          <div className="control-group">
+            <label className="control-label" forhtml="inputPassword">Password</label>
+            <div className="controls">
+              <input  value={this.state.password} onChange={this.handleChange} type="password" id="inputPassword" placeholder="Password" />
+            </div>
           </div>
-          <button type="submit" onClick={this.login} className="btn btn-primary">Login</button>
-          <button onClick={this.signup} style={{marginLeft: '25px'}} className="btn btn-success">Signup</button>
-        </form>
-      
+          <div className="control-group">
+            <div className="controls">
+            {/* <button type="submit" onClick={this.login} className="btn btn-primary">Login</button> */}
+          <button onClick={this.signup} className="btn">Sign Up</button>
+          </div>
+          </div>
+        </form> 
+    
       </div>
     );
   }
